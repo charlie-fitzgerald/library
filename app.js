1416bbb64fa5bf8279d947ff45190062005bb63c
Charlie Fitzgerald <46912450+charlie-fitzgerald@users.noreply.github.com>
2023-06-29 15:05:20 -0700

    Connect buttons, add hidden form

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const myLibrary = [];
+const displayFormButton = document.querySelector(".display-form-button");
+const submitBookBtn = document.querySelector(".submit-book-btn")
+const formWrapper = document.querySelector(".form-wrapper");
+const newBookForm = document.querySelector(".new-book-form");
+
+displayFormButton.addEventListener("click", e => {
+    e.preventDefault();
+    formWrapper.classList.remove('hidden');
+})
+
+submitBookBtn.addEventListener("click", e => {
+    e.preventDefault();
+    formWrapper.classList.add("hidden");
+})
+
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
