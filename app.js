588d2b26a0db6a0e88eaa223a392daa042aa78e7
Charlie Fitzgerald <46912450+charlie-fitzgerald@users.noreply.github.com>
2023-07-14 16:39:27 -0700

    Stuff

--- app.js
+++ app.js
@@ -2,23 +2,59 @@ const displayFormButton = document.querySelector(".display-form-button");
 const submitBookBtn = document.querySelector(".submit-book-btn")
 const formWrapper = document.querySelector(".form-wrapper");
 const newBookForm = document.querySelector(".new-book-form");
+const libraryGrid = document.querySelector(".library-grid");
 
-displayFormButton.addEventListener("click", e => {
-    e.preventDefault();
-    formWrapper.classList.remove('hidden');
-})
+let myLibrary = [
+  
+  {title: 'The Stand',
+  author: 'Stephen King',
+  pages: 1100,
+  read: true},
 
-submitBookBtn.addEventListener("click", e => {
-    e.preventDefault();
-    formWrapper.classList.add("hidden");
-})
+  {title: 'Pride and Prejudice',
+  author: 'Jane Austen',
+  pages: 1000000,
+  read: false
+  }
 
-let myLibrary = [];
+];
 
-function Book() {
-  // the constructor...
+const displayBooks = arr => {
+  arr.forEach(book => {
+    const bookCard = document.createElement("div");
+    const para = document.createElement('p');
+    const title = book.title;
+    const author = book.author;
+    const pages = book.pages;
+    const read = book.read;
+
+    para.innerHTML = title;
+    bookCard.appendChild(para);
+
+    libraryGrid.appendChild(bookCard);
+  })
+}
+
+
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
   // do stuff here
 }
+
+displayFormButton.addEventListener("click", e => {
+  e.preventDefault();
+  formWrapper.classList.remove('hidden');
+  displayBooks(myLibrary);
+})
+
+submitBookBtn.addEventListener("click", e => {
+  e.preventDefault();
+  formWrapper.classList.add("hidden");
+})
